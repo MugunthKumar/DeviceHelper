2b9debc4d1f59c55efd4c0d944b9f3076b651bcd
Mugunth Kumar <mugunth.kumar@gmail.com>
2010-08-15 22:07:26 +0800

    Initial Commit

---
+++ Classes/DeviceHelperAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  DeviceHelperAppDelegate.h
+//  DeviceHelper
+//
+//  Created by Mugunth Kumar on 15-Aug-10.
+//  Copyright Steinlogic 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DeviceHelperViewController;
+
+@interface DeviceHelperAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    DeviceHelperViewController *viewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet DeviceHelperViewController *viewController;
+
+@end
+
---
+++ Classes/DeviceHelperViewController.h
@@ -0,0 +1,16 @@
+//
+//  DeviceHelperViewController.h
+//  DeviceHelper
+//
+//  Created by Mugunth Kumar on 15-Aug-10.
+//  Copyright Steinlogic 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeviceHelperViewController : UIViewController {
+
+}
+
+@end
+
